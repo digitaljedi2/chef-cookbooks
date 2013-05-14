0581e606b8af21e50ef34bbc1425d2b372e839ce
Kyle Burton <kyle.burton@gmail.com>
2013-05-14 15:28:34 -0400

    build for ejabberd

--- ejabberd/attributes/default.rb
+++ ejabberd/attributes/default.rb
@@ -2,6 +2,16 @@ default['ejabberd']['version']['major'] = '2'
 default['ejabberd']['version']['minor'] = '1'
 default['ejabberd']['version']['incremental'] = '12'
 
+default["ejabberd"]["path"] = '/usr/local/ejabberd'
+default["ejabberd"]["install_path"]   = [
+  '/usr/local/ejabberd-',
+  default['ejabberd']['version']['major'],
+  ".",
+  default['ejabberd']['version']['minor'],
+  ".",
+  default['ejabberd']['version']['incremental'],
+].join('')
+
 # NB: I created the sha1 manually after I downloaded 2.1.12, no checksum
 # files seemed to be available at the process-one.net website
 default['ejabberd']['source']['checksum'] = "5610a944afb8ac07e68ca87d14ef398a0d42d633"
--- ejabberd/recipes/source.rb
+++ ejabberd/recipes/source.rb
@@ -1,30 +1,38 @@
 include_recipe "build-essential"
 
-ejabber_url = node['ejaberd']['source']['url']
-ejabber_pkg_fname = File.basename(ejabber_url)
-ejabber_dname = File.basename(ejabber_url, ".tgz")
+ejabber_url          = node['ejabberd']['source']['url']
+ejabber_pkg_fname    = File.basename(ejabber_url)
+ejabber_dname        = File.basename(ejabber_url, ".tgz")
+ejabber_path         = node['ejabberd']['path']
+ejabber_install_path = node['ejabberd']['install_path']
 
 file_cache_path = Chef::Config[:file_cache_path]
+ejabber_remote_file = File.join(file_cache_path, ejabber_pkg_fname) 
 
-remote_file File.join(file_cache_path, ejabber_pkg_fname) do
-  source ejabber_url
-  owner "root"
-  mode 0644
-  checksum node['erlang']['source']['checksum']
-  notifies :run, "bash[install-erlang]", :immediately
-  action :create_if_missing
-end
-
-
-# TODO: idempotent unarchive ejabberd
-# TODO: idempotent configure for ejabberd
-# TODO: idempotent build for ejabberd
-# TODO: idempotent install for ejabberd
-
-bash "install-erlang" do
-  action :none
+bash "install-ejabberd" do
+  # NB: run immediately if the file has already been downloaded
+  if ::File.exists? ejabber_remote_file
+    action  :run
+  else
+    action  :nothing
+  end
   cwd     file_cache_path
   code <<-END
 test -d #{ejabber_dname} || tar xzvf #{ejabber_pkg_fname}
+cd #{ejabber_dname}/src
+test -f Makefile ./configure --prefix=#{ejabber_install_path}
+test -f #{ejabber_install_path}/sbin/ejabberdctl make && make install
+test -f #{ejabber_path} || ln -s #{ejabber_install_path} #{ejabber_path}
   END
 end
+
+Chef::Log.info "configuring remote file: #{ejabber_remote_file}"
+remote_file ejabber_remote_file do
+  source ejabber_url
+  owner "root"
+  mode 0644
+  checksum node['ejabberd']['source']['checksum']
+  notifies :run, "bash[install-ejabberd]", :immediately
+  action :create_if_missing
+end
+
