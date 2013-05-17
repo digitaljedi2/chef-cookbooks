bd86c85fac5c331ab9e7db999328e7be3c4b2d0c
Kyle Burton <kyle.burton@gmail.com>
2013-05-17 14:40:34 -0400

    working install

--- ejabberd/recipes/source.rb
+++ ejabberd/recipes/source.rb
@@ -6,23 +6,54 @@ ejabber_dname        = File.basename(ejabber_url, ".tgz")
 ejabber_path         = node['ejabberd']['path']
 ejabber_install_path = node['ejabberd']['install_path']
 
+user "ejabberd" do
+  action :create
+  home ejabber_install_path
+  shell "/bin/bash"
+end
+
 file_cache_path = Chef::Config[:file_cache_path]
 ejabber_remote_file = File.join(file_cache_path, ejabber_pkg_fname) 
 
 bash "install-ejabberd" do
   # NB: run immediately if the file has already been downloaded
   if ::File.exists? ejabber_remote_file
+    Chef::Log.info "local file already exists, will register install with action :run"
     action  :run
   else
+    Chef::Log.info "local does not already exist, will register install with action :nothing"
     action  :nothing
   end
   cwd     file_cache_path
   code <<-END
-test -d #{ejabber_dname} || tar xzvf #{ejabber_pkg_fname}
-cd #{ejabber_dname}/src
-test -f Makefile ./configure --prefix=#{ejabber_install_path}
-test -f #{ejabber_install_path}/sbin/ejabberdctl make && make install
-test -h #{ejabber_path} || ln -s #{ejabber_install_path} #{ejabber_path}
+date >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+if [ -d #{ejabber_dname} ]; then
+  echo "archive already extracted" >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+else
+  tar xzvf #{ejabber_pkg_fname} >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+fi
+
+cd #{ejabber_dname}/src || echo "Whoops, #{ejabber_dname}/src doesn't exist?" >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+if [ -f Makefile ]; then
+  echo "Makefile exists, configure must have been run" >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+else
+  ./configure --prefix=#{ejabber_install_path} >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+fi
+
+if [ -f #{ejabber_install_path}/sbin/ejabberdctl ]; then
+  echo "#{ejabber_install_path}/sbin/ejabberdctl exists, install must have been done" >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+else
+  (make && make install)    >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+fi
+
+if [ -h #{ejabber_path} ]; then
+  echo "#{ejabber_path} symlink already exists, all done." >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+else
+  ln -s #{ejabber_install_path} #{ejabber_path} >> #{Chef::Config[:file_cache_path]}/ejabberd.out
+fi
+
+chown -R ejabberd.ejabberd #{ejabber_install_path}
+
   END
 end
 
